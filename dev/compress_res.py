7b728a8a05e5c67eb68fe3dd7d32c8e5278e504c
Thomas Williams <thomas.williams@astro.cf.ac.uk>
2018-09-12 10:41:48 +0100

    20180912 Speeding up compress_res.py

--- dev/compress_res.py
+++ dev/compress_res.py
@@ -11,49 +11,69 @@ import glob
 import numpy as np
 from tqdm import tqdm
 
-os.chdir('/home/daedalusdata/c1625914/dustem_fitter/dev/grid')
+os.chdir(os.getcwd())
 
-#Set up a pandas DataFrame for each component (small- and large-carbons,
-#and silicates)
-
-sCM20_df = pd.DataFrame()
-lCM20_df = pd.DataFrame()
-aSilM5_df = pd.DataFrame()
+#First, create the names
 
 alpha = np.arange(2.6,5.41,0.01)
 isrf = np.arange(-1,3.51,0.01)
 
-for i in tqdm(alpha,
-              desc='Total'):
-    for j in tqdm(isrf,
-                  leave=False,
-                  desc='alpha=%.2f' % i):
-    
-        small_grains,\
-            large_grains,\
-            pyroxine,\
-            olivine, = np.loadtxt('SED_%.2f_%.2f.RES' % (i,j),
-                                  unpack=True,
-                                  skiprows=8,
-                                  usecols=(1,2,3,4))
-            
-        if i == alpha[0] and j == isrf[0]:
-            sCM20_df['wavelength'] =  np.loadtxt('SED_%.2f_%.2f.RES' % (i,j),
-                                                 unpack=True,
-                                                 skiprows=8,
-                                                 usecols=(0))
-    
-        identifier = '%.2f,%.2f' % (i,j)
-        
-        sCM20_df[identifier] = small_grains
-        lCM20_df[identifier] = large_grains
-        aSilM5_df[identifier] = pyroxine+olivine
+names = []
 
-sCM20_df.to_hdf('/home/daedalusdata/c1625914/dustem_fitter/dustem_res.h5',
-                mode='w',key='sCM20')
-lCM20_df.to_hdf('/home/daedalusdata/c1625914/dustem_fitter/dustem_res.h5',
-                key='lCM20')
-aSilM5_df.to_hdf('/home/daedalusdata/c1625914/dustem_fitter/dustem_res.h5',
-                key='aSilM5')
+for i in alpha:
+    for j in isrf:
+        
+        names.append('%.2f,%.2f' % (i,j))
+        
+        #And read in the wavelength
+        
+        if i == alpha[0] and j == isrf[0]:
+            
+            wavelength = np.loadtxt('SED_%.2f_%.2f.RES' % (i,j),
+                                    unpack=True,
+                                    skiprows=8,
+                                    usecols=(0))
+
+# for i in tqdm(alpha,
+#               desc='Total'):
+#     for j in tqdm(isrf,
+#                   leave=False,
+#                   desc='alpha=%.2f' % i):
+#     
+#         small_grains,\
+#             large_grains,\
+#             pyroxine,\
+#             olivine, = np.loadtxt('SED_%.2f_%.2f.RES' % (i,j),
+#                                   unpack=True,
+#                                   skiprows=8,
+#                                   usecols=(1,2,3,4))
+#             
+#         if i == alpha[0] and j == isrf[0]:
+#             sCM20_df['wavelength'] =  np.loadtxt('SED_%.2f_%.2f.RES' % (i,j),
+#                                                  unpack=True,
+#                                                  skiprows=8,
+#                                                  usecols=(0))
+#     
+#         identifier = '%.2f,%.2f' % (i,j)
+#         
+#         sCM20_df[identifier] = small_grains
+#         lCM20_df[identifier] = large_grains
+#         aSilM5_df[identifier] = pyroxine+olivine
+        
+#Set up a pandas DataFrame for each component (small- and large-carbons,
+#and silicates)
+
+sCM20_df = pd.DataFrame(columns=names,data=np.zeros([len(wavelength),len(names)]))
+lCM20_df = pd.DataFrame()
+aSilM5_df = pd.DataFrame()
+
+print(sCM20_df)
+
+# sCM20_df.to_hdf('test.h5',
+#                 mode='w',key='sCM20')
+# lCM20_df.to_hdf('test.h5',
+#                 key='lCM20')
+# aSilM5_df.to_hdf('test.h5',
+#                 key='aSilM5')
 
 print('Complete!')
