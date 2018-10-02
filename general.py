20181002 fixed bug where stellar scaling was being locked to longest wavelength

general.py
@@ -61,7 +61,7 @@ def define_stars(flux_df,
                  idx_key):
     
     #Create a blackbody of 5000K to represent the stars, and lock this
-    #at the 3.6micron IRAC flux (or the 3.4 WISE flux if not available)
+    #to the shortest wavelength
     
     stars = 2*h*frequency**3/c**2 * (np.exp( (h*frequency)/(k*5000) ) -1)**-1
     

plotting.py
@@ -121,7 +121,7 @@ def plot_sed(method,
     obs_wavelength = np.array(obs_wavelength)
     obs_flag = np.array(obs_flag)
     
-    idx = np.where( obs_wavelength[obs_flag == 0] == np.max(obs_wavelength[obs_flag == 0]) )
+    idx = np.where( obs_wavelength[obs_flag == 0] == np.min(obs_wavelength[obs_flag == 0]) )
     idx_key = keys[idx[0][0]]
     
     #Generate stars

themcmc_sampler.py
@@ -147,7 +147,9 @@ def sample(method,
     obs_error = np.array(obs_error)
     obs_wavelengths = np.array(obs_wavelengths)
     
-    idx = np.where( obs_wavelengths == np.max(obs_wavelengths) )
+    #Define stars, locking the initial scaling guess to shortest wavelength
+    
+    idx = np.where( obs_wavelengths == np.min(obs_wavelengths) )
     idx_key = keys[idx[0][0]]
     
     stars = general.define_stars(flux_df,
@@ -158,6 +160,9 @@ def sample(method,
     
     #Set an initial guess for the scaling variable. Lock this to the longest flux
     #available
+    
+    idx = np.where( obs_wavelengths == np.max(obs_wavelengths) )
+    idx_key = keys[idx[0][0]]
     
     idx = np.where(np.abs(wavelength-filter_df[idx_key][0]) == np.min(np.abs(wavelength-filter_df[idx_key][0])))
     